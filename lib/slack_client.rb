46c3fcccb570a5c1d9093139257c569f60fc61e3
Mitchell Bagley <bagleymj@gmail.com>
2016-03-16 15:01:59 -0400

    Decided to leave it sloppily persistent for now.

--- lib/slack_client.rb
+++ lib/slack_client.rb
@@ -1,22 +1,25 @@
-require_relative '../environment.rb'
-require 'open-uri'
-require 'json'
-require 'websocket-client-simple'
-
 class SlackClient
+  require_relative '../environment.rb'
+  require 'open-uri'
+  require 'json'
+  require 'websocket-client-simple'
+
   def initialize
     @bot_token = SLACK_BOT_TOKEN
+    @console = nil
   end
+
+  ws = nil
 
   def connect
     response = open('https://slack.com/api/rtm.start?token='+ @bot_token + '&pretty=1').read
     rtm = JSON.parse(response)
     url = rtm['url']
-    ws = WebSocket::Client::Simple.connect url
-    maintain_session_for ws
+    @ws = WebSocket::Client::Simple.connect url
+    maintain_session_for ws, num
   end
 
-  def maintain_session_for(ws)
+  def maintain_session_for(ws, num)
     ws.on :open do
       puts "CONNECTED!"
       puts "To exit simply type 'exit' in the console and press ENTER."
@@ -38,6 +41,7 @@ class SlackClient
         console = gets.chomp.downcase
         if console == "exit"
           ws.close
+          threads[1].kill
           break
         end
       end
@@ -45,9 +49,10 @@ class SlackClient
     threads << Thread.new {
       sleep 30
       puts "refreshing connection"
-      ws.close
       threads[0].kill
+      ws.close
       connect
+
     }
     threads.each do |thread|
       thread.join
